b3f1946edc5d91550dbd053e3d24ad4de12b5558
Ali Abuzahra <aliabuzahra@Alis-MacBook-Air.local>
2025-11-07 18:26:09 +0300

    Migrate from SQL Server to PostgreSQL

--- ShopFree.Infrastructure/DependencyInjection.cs
+++ ShopFree.Infrastructure/DependencyInjection.cs
@@ -17,7 +17,7 @@ public static class DependencyInjection
     {
         // Database
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(
+            options.UseNpgsql(
                 configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
